1fba246785cdcd8e3f015002ef234f4053e969cf
saharsamr <sahar.rajabi76@gmail.com>
2019-01-09 22:26:22 +0330

    some refactoring

---
+++ State.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+
+class State(Enum):
+    first = 1
+    second = 2
+    third = 3
+    forth = 4
--- network_encoding.py
+++ network_encoding.py
@@ -1,11 +1,4 @@
-from enum import Enum
-
-
-class State(Enum):
-    first = 1
-    second = 2
-    third = 3
-    forth = 4
+from State import State
 
 
 def encode(string):
